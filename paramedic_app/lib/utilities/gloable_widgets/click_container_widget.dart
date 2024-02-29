522d1a4eb7fa71fb3c6dd19a62d36a35840de1e8
Rahaf Alqahtani <86989761+rahafwmaq@users.noreply.github.com>
2024-02-29 17:57:38 +0300

    Parameedic UI is ALL DONE ;)

--- paramedic_app/lib/utilities/gloable_widgets/click_container_widget.dart
+++ paramedic_app/lib/utilities/gloable_widgets/click_container_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:paramedic_app/utilities/gloable_widgets/text_widget.dart';
+import 'package:paramedic_app/utilities/helpers/screen_size.dart';
 import 'package:paramedic_app/view_layout/color.dart';
 
 class ClickContainerWidget extends StatelessWidget {
@@ -21,13 +22,13 @@ class ClickContainerWidget extends StatelessWidget {
     return InkWell(
       onTap: onTap,
       child: Container(
-        height: 46,
-        width: 200,
+        height: context.getHeight(divide: 18),
+        width: context.getWidth(divide: 2),
         decoration: BoxDecoration(
             color: color,
             borderRadius: BorderRadius.circular(20),
             boxShadow: const [
-              BoxShadow(color: lightGrey, blurRadius: 20),
+              BoxShadow(color: lightGrey, blurRadius: 10),
             ]),
         child: Center(
             child: TextWidget(
---
+++ paramedic_app/lib/views/paramedic_view/key_body_digram.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:paramedic_app/utilities/gloable_widgets/text_widget.dart';
+import 'package:paramedic_app/utilities/helpers/screen_size.dart';
+import 'package:paramedic_app/view_layout/color.dart';
+import 'package:paramedic_app/view_layout/sizebox.dart';
+
+class KeyBodyDigram extends StatelessWidget {
+  const KeyBodyDigram(
+      {super.key,
+      required this.positionTop,
+      required this.positionLeft,
+      required this.keyName, required this.color});
+
+  final double positionTop;
+  final double positionLeft;
+  final String keyName;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: context.getHeight(divide: positionTop),
+      left: context.getWidth(divide: positionLeft),
+      child: Row(
+        children: [
+          Container(
+            height: context.getHeight(divide: 20),
+            width: context.getWidth(divide: 10),
+            decoration: BoxDecoration(
+                color: color, borderRadius: BorderRadius.circular(20)),
+          ),
+          width10,
+          TextWidget(text: keyName)
+        ],
+      ),
+    );
+  }
+}
