a6a90e67ce63671554afea11674cdb7c34dd0592
senshi900 <dr.fisal99@gmail.com>
2024-03-02 22:53:31 +0300

    fix qr reader

--- paramedic_app/lib/views/paramedic_view/paramedic_home_view.dart
+++ paramedic_app/lib/views/paramedic_view/paramedic_home_view.dart
@@ -1,6 +1,8 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:paramedic_app/database/supa_get_delete/supa_get_delete.dart';
+import 'package:paramedic_app/globals/global.dart';
 import 'package:paramedic_app/models/paramedic_model.dart';
+import 'package:paramedic_app/models/patient_model.dart';
 import 'package:paramedic_app/utilities/gloable_widgets/click_container_widget.dart';
 import 'package:paramedic_app/utilities/gloable_widgets/text_form_field_widget.dart';
 import 'package:paramedic_app/utilities/gloable_widgets/text_widget.dart';
@@ -8,15 +10,67 @@ import 'package:paramedic_app/utilities/helpers/navigator.dart';
 import 'package:paramedic_app/utilities/helpers/screen_size.dart';
 import 'package:paramedic_app/view_layout/color.dart';
 import 'package:paramedic_app/view_layout/sizebox.dart';
-import 'package:paramedic_app/views/paramedic_auth_view/paramedic_login_view.dart';
 import 'package:paramedic_app/views/paramedic_view/paramedic_profile_pic.dart';
 import 'package:paramedic_app/views/paramedic_view/patient_view_for_paramedic.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
-class ParamedicHomeView extends StatelessWidget {
-  ParamedicHomeView({super.key, required this.paramedic});
-  final Paramedic paramedic;
+class ParamedicHomeView extends StatefulWidget {
+  const ParamedicHomeView({super.key, required Paramedic paramedic});
 
+  @override
+  State<ParamedicHomeView> createState() => _ParamedicHomeViewState();
+}
+
+class _ParamedicHomeViewState extends State<ParamedicHomeView> {
   final TextEditingController patientIDController = TextEditingController();
+  bool iserror = false;
+  Future<void> scanBarcode() async {
+    String barcodeScanRes;
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        "#ff6666",
+        "Cancel",
+        true,
+        ScanMode.QR,
+      );
+      if (!mounted) return;
+
+      if (barcodeScanRes != '-1') {
+        setState(() {
+          patientIDController.text = barcodeScanRes;
+          iserror = false;
+        });
+        fetchAndNavigate();
+      }
+    } catch (e) {
+      setState(() {
+        iserror = true;
+      });
+    }
+  }
+
+  Future<void> fetchAndNavigate() async {
+    try {
+      final Patient myPatient =
+          await SupaGetAndDelete().getPatientById(patientIDController.text);
+      globalCurrentPatient = myPatient;
+      print(globalCurrentPatient!.fullName!);
+      if (myPatient.id != null) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
+                PatientViewForParamedic(idText: patientIDController.text),
+          ),
+        );
+      }
+    } catch (error) {
+      print("Error: $error");
+      setState(() {
+        iserror = true;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,62 +88,33 @@ class ParamedicHomeView extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconButton(
-                        onPressed: () {
-                          if (context.locale.toString() == "en_US") {
-                            context.setLocale(const Locale('ar', 'SA'));
-                          } else {
-                            context.setLocale(const Locale('en', 'US'));
-                          }
-                        },
-                        icon: const Icon(
-                          Icons.translate_rounded,
-                          color: red,
-                          size: 30,
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () {
-                          context.removeUntil(view: ParamedicLoginView());
-                        },
-                        icon: const Icon(
-                          Icons.logout,
-                          color: red,
-                          size: 30,
-                        ),
-                      )
-                    ],
-                  ),
                   const ParamedicProfilePic(),
                   height10,
-                  TextWidget(
-                    text: '${'ParamedicRegistrationScreen.ID'.tr()} 101',
+                  const TextWidget(
+                    text: 'ID : 103',
                     fontSize: 18,
                     fontWeight: FontWeight.bold,
                   ),
-                  TextWidget(
-                    text: paramedic.name!,
+                  const TextWidget(
+                    text: 'Faisal Alshamary',
                     fontSize: 18,
                     fontWeight: FontWeight.w400,
                   ),
-                  TextWidget(
-                    text: paramedic.email!,
+                  const TextWidget(
+                    text: 'Dr.fisal99@gmail.com',
                     fontSize: 18,
                     fontWeight: FontWeight.w400,
                   ),
-                  TextWidget(
-                    text: paramedic.hospital!,
+                  const TextWidget(
+                    text: 'King Abdulaziz Hosital',
                     fontSize: 18,
                     fontWeight: FontWeight.w400,
                   ),
                   height70,
-                  Row(
+                  const Row(
                     children: [
                       TextWidget(
-                        text: 'ParamedicRegistrationScreen.serachby_id'.tr(),
+                        text: 'Search by Patient ID :',
                         fontSize: 18,
                         textColor: red,
                         fontWeight: FontWeight.bold,
@@ -100,7 +125,7 @@ class ParamedicHomeView extends StatelessWidget {
                   TextFormFieldWidget(
                     keyboardType: TextInputType.text,
                     controller: patientIDController,
-                    labelText: 'ParamedicRegistrationScreen.patient_id'.tr(),
+                    labelText: 'Patient ID',
                     labelTextColor: red,
                     controllerTextColor: black,
                     cursorColor: red,
@@ -114,32 +139,62 @@ class ParamedicHomeView extends StatelessWidget {
                   ClickContainerWidget(
                     onTap: () {
                       context.pushView(
-                          view: PatientViewForParamedic(
-                        idText: patientIDController.text,
+                          view: const PatientViewForParamedic(
+                        idText: "1abe2dce-529f-49dd-8b1d-c3f7af31233d",
                       ));
                     },
                     color: blueTransit,
-                    text: 'ParamedicRegistrationScreen.buttonText'.tr(),
+                    text: 'Submit',
                     textColor: white,
                     fontSize: 20,
                   ),
                   height20,
-                  TextWidget(
-                    text: 'ParamedicRegistrationScreen.or'.tr(),
-                    fontSize: 20,
-                  ),
+                  const TextWidget(text: 'OR'),
                   height20,
                   Row(
                     children: [
-                      TextWidget(
-                        text: 'ParamedicRegistrationScreen.scan_qr'.tr(),
+                      const TextWidget(
+                        text: 'Scan QR Code :',
                         fontSize: 18,
                         textColor: red,
                         fontWeight: FontWeight.bold,
                       ),
                       width80,
                       InkWell(
-                        onTap: () {},
+                        onTap: () async {
+                          try {
+                            scanBarcode();
+
+                            print(globalCurrentPatient!.fullName!);
+                            if (globalCurrentPatient!.fullName != null) {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => PatientViewForParamedic(
+                                      idText: patientIDController.text),
+                                ),
+                              );
+                            }
+                          } catch (e) {
+                            print(e);
+                            setState(() {
+                              iserror = true;
+                            });
+                            showDialog(
+                                context: context,
+                                barrierDismissible: false,
+                                builder: (context) {
+                                  Future.delayed(
+                                      const Duration(milliseconds: 500), () {
+                                    Navigator.of(context).pop(true);
+                                  });
+
+                                  return const Center(
+                                      child: CircularProgressIndicator(
+                                          color: red));
+                                });
+                          }
+                        },
                         child: Container(
                           height: context.getHeight(divide: 7),
                           width: context.getWidth(divide: 3),
